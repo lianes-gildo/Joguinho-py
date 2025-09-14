e24cd8b8aef7ab3aad9ea355c415f99045b7b97f
Lianes Nhacula <lianesnhacula03@gmail.com>
2025-09-14 10:31:32 +0200

    Criacao dos obstaculos do joguinho

--- main.py
+++ main.py
@@ -10,3 +10,9 @@ clock = pygame.time.Clock()
 jogador = pygame.Rect(180, 500, 40, 40)
 velocidade = 5
 obstaculos = []
+
+def criar_obstaculos():
+    x = random.randint(0, largura - 40)
+    return pygame.Rect(x, -40, 40, 40)
+
+rodando = True
